3a1ae773eed58819747e8adb8db93fac8f41a594
RenJie0813 <924467352@qq.com>
2018-06-08 11:58:28 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
+<<<<<<< HEAD
 # Vue2-WxChat
 vue2开发的仿微信，支持pc和移动端，图片压缩修复处理，交互逼真，多页路由，666。
+=======
+<<<<<<< HEAD
+# Vue2-WxChat
+vue2开发的仿微信，支持pc和移动端，图片压缩修复处理，交互逼真，多页路由，666。
+=======
+# project
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+>>>>>>> add
+>>>>>>> add files
---
+++ src/pages/Index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="wechat">
+    <search></search>
+    
+    <ul class="wechat-list" v-show="!getHeaderStatus">
+      <!--props传递消息对象 baseMsgObj -->
+      <chat-item v-on:delete="deleteOne" v-for="baseMsgObj in getChatList" :item="baseMsgObj"
+        class="list-row line-bottom" :key="baseMsgObj.chatId"></chat-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import chatItem from '../components/chatItem.vue'
+  import search from '../components/search.vue'
+  import {
+    mapGetters
+  } from 'vuex'
+
+  export default {
+    data() {
+      return {
+      }
+    },
+    computed: {
+      ...mapGetters(['getHeaderStatus','getChatList'])
+    },
+    methods: {
+      deleteOne(data) {
+        this.$store.dispatch("delChatItem",data.chatId);
+      }
+    },
+    mounted() {
+
+    },
+    created() {
+    },
+    components: {
+      chatItem,
+      search
+    },
+    // beforeRouteLeave: function (to, from, next) {
+    //   window.c = document.body.scrollTop;
+    //   next();
+    // },
+    // beforeRouteEnter: function (to, from, next) {
+    //   next();
+    //   setTimeout(function () {
+    //     document.body.scrollTop = window.c;
+    //   }, 0)
+    // }
+  }
+</script>
+
+<style>
+  @import "../../static/css/chat.css";
+</style>
